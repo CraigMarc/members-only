2bf2fcc38676e90edb2f7a2d06ce22171993c94d
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-08 18:07:37 -0500

    added message form view

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,9 @@ router.get('/log-in', function(req, res, next) {
   res.render('log-in', { title: 'Log In' });
 });
 
+//get message form
+router.get('/message-form', function(req, res, next) {
+  res.render('message-form', { title: 'New Message' });
+});
+
 module.exports = router;
