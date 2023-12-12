c8855e01849bc067c3ab08b435c81449c73027b3
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-12 18:24:39 -0500

    prepared for deploy

--- app.js
+++ app.js
@@ -16,8 +16,28 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require('bcryptjs')
+const compression = require("compression");
+const helmet = require("helmet");
 
-var app = express();
+const app = express();
+
+app.use(compression()); // Compress all routes
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 const mongoDB = process.env.MONGODB_URI
 
